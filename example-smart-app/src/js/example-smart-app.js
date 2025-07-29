6f633d6011555002faa0858b86e9767ebcf3f9a0
punit110188 <168558896+punit110188@users.noreply.github.com>
2025-07-29 10:09:17 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -134,7 +134,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    fetchAndRenderClinicalTrials();
+    fetchTrialsV2();
   };
 
   function fetchTrialsV2() {
